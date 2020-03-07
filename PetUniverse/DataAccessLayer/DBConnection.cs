e7a13af1341eb0a6e942b46f0dfeec05cba922e7
intelCore307 <jaeho-kim@student.kirkwood.edu>
2020-03-06 18:15:20 -0600

    Fixed transaction UI

--- PetUniverse/DataAccessLayer/DBConnection.cs
+++ PetUniverse/DataAccessLayer/DBConnection.cs
@@ -20,11 +20,11 @@ namespace DataTransferObjects
 
 
         // Connection String for home
-        //@"Data Source=localhost\sqlexpress;Initial Catalog=PetUniverseDB; Integrated Security = True";
+        @"Data Source=localhost\sqlexpress;Initial Catalog=PetUniverseDB; Integrated Security = True";
 
 
         //Connection string for school
-        @"Data Source=localhost;Initial Catalog=PetUniverseDB;Integrated Security=True"; // Connection string for school
+        //@"Data Source=localhost;Initial Catalog=PetUniverseDB;Integrated Security=True"; // Connection string for school
 
 
         //@"Data Source=LAPTOP-T3PUJGNB\SQLEXPRESS;Initial Catalog=PetUniverseDB;Integrated Security=True";
