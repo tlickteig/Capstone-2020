8b3f0efb09303b2ba3345f51ce02836516b7e9be
unknown <zbehrensmeyer@yahoo.com>
2020-04-21 22:16:19 -0700

    Adding Identity System Part 1

--- PetUniverse/WPFPresentation/Models/IdentityModels.cs
+++ PetUniverse/WPFPresentation/Models/IdentityModels.cs
@@ -8,6 +8,8 @@ namespace WPFPresentation.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,7 +22,7 @@ namespace WPFPresentation.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        : base("IdentityConnection", throwIfV1Schema: false)
         {
         }
 
