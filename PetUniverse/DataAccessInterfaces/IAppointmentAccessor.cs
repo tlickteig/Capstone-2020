7957b138e72497864a8356bd4245532635f8f4e2
Mohamed E Elamin <moalsir89@gmail.com>
2020-04-23 03:40:27 -0500

    Facilitator Welcome basket

--- PetUniverse/DataAccessInterfaces/IAppointmentAccessor.cs
+++ PetUniverse/DataAccessInterfaces/IAppointmentAccessor.cs
@@ -5,7 +5,7 @@ namespace DataAccessInterfaces
 {
     /// <summary>
     /// Creator: Thomas Dupuy
-    /// Created: 02/06/2020
+    /// Created: 2020/02/06
     /// Approver: Awaab Elamin
     /// 
     /// This interface class is used as an interface for the Accessor Layer
@@ -14,72 +14,86 @@ namespace DataAccessInterfaces
     {
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 02/06/2020
+        /// Created: 2020/02/06
         /// Approver: Awaab Elamin
-        /// 
         /// This method selects all appointments
         /// </summary>        
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments. 
+        ///  And updated the date format. 
         /// </remarks>
+        /// <returns>A list of active appointments</returns>
         List<AppointmentLocationVM> SelectAllActiveAppointments();
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/12/2020
+        /// Created: 2020/04/12
         /// Approver: Michael Thompson
-        /// 
         /// This method selects an appointment by its id
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments.
+        /// And updated the date format. 
         /// </remarks>
+        /// <param name="id"></param>
+        /// <returns> An appointment by appointment ID</returns>
         AppointmentLocationVM SelectAppointmentByID(int id);
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/12/2020
+        /// Created: 2020/04/12
         /// Approver: Michael Thompson
         /// 
         /// This method deactivates an appointment
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments.
+        /// And updated the date format. 
         /// </remarks>
+        /// <param name="appointment"></param>
+        /// <returns>rows count</returns>
         int DeactivateAppointment(Appointment appointment);
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/12/2020
+        /// Created:2020/04/12
         /// Approver: Michael Thompson
         /// 
         /// This method inserts an appointment
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments.
+        /// updated the date format .
         /// </remarks>
+        /// <param name="appointment"></param>
+        /// <param name=""></param>
+        /// <returns>rows count</returns>
         int InsertAppointment(Appointment appointment);
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/15/2020
+        /// Created:2020/04/12
         /// Approver: Michael Thompson
         /// 
         /// This method updates an appointment
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments.
+        /// And  updated the date format. 
         /// </remarks>
+        /// <param name="oldAppointment"></param>
+        /// <param name="newAppointment"></param>
+        /// <returns>rows count</returns>
         int UpdateAppointment(Appointment oldAppointment, Appointment newAppointment);
     }
 }
--- PetUniverse/LogicLayerInterfaces/IAppointmentManager.cs
+++ PetUniverse/LogicLayerInterfaces/IAppointmentManager.cs
@@ -5,7 +5,7 @@ namespace LogicLayerInterfaces
 {
     /// <summary>
     /// Creator: Thomas Dupuy
-    /// Created: 02/06/2020
+    /// Created: 2020/02/06
     /// Approver: Awaab Elamin
     /// 
     /// This interface class is used as an interface for the logic layer
@@ -16,71 +16,77 @@ namespace LogicLayerInterfaces
         /// Creator: Thomas Dupuy
         /// Created: 2020/02/06
         /// Approver: Awaab Elamin
-        /// 
         /// This method retrieves all appointments
         /// </summary>
-        ///
         /// <remarks>
-        /// Updater: 
-        /// Updated:  
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments. 
         /// </remarks>
+        /// <returns>A list of active appointments</returns>
         List<AppointmentLocationVM> RetrieveAllActiveAppointments();
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/12/2020
-        /// Approver: Michael Thompson
-        /// 
+        /// Created: 2020/04/12
+        /// Approver: Michael Thompson 
         /// This method retrieves an appointment by its id
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments. And updated date format. 
         /// </remarks>
+        /// <param name="id"></param>
+        /// <returns> An appointment by appointment ID</returns>
         AppointmentLocationVM RetrieveAppointmentByID(int id);
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/12/2020
+        /// Created: 2020/04/12
         /// Approver: Michael Thompson
-        /// 
         /// This method removes an appointment
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments. And updated date format. 
         /// </remarks>
+        /// <param name="appointment"></param>
+        /// <returns>rows count</returns>
         int RemoveAppointment(Appointment appointment);
+
 
         /// <summary>
         /// Creator: Thomas Dupuy
-        /// Created: 4/12/2020
+        /// Created: 2020/04/12
         /// Approver: Michael Thompson
         /// 
         /// This method adds an appointment
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments. 
         /// </remarks>
+        /// <param name="appointment"></param>
+        /// <returns>rows count</returns>
         int AddAppointment(Appointment appointment);
 
         /// <summary>
         /// Creator: Thomas Dupuy
         /// Created: 4/15/2020
         /// Approver: Michael Thompson
-        /// 
         /// This method updates an appointment
         /// </summary>
         /// <remarks>
-        /// Updater: 
-        /// Updated: 
-        /// Update: 
+        /// Updater: Mohamed Elamin 
+        /// Updated: 2020/04/20
+        /// Update: I Added param, returns tags for the comments.  And updated date format 
         /// </remarks>
+        /// <param name="oldAppointment"></param>
+        /// <param name="newAppointment"></param>
+        /// <returns>rows count</returns>
         int EditAppointment(Appointment oldAppointment, Appointment newAppointment);
     }
 }
