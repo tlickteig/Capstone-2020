670537ef7323b601a425d95152430cb42c59e74c
Steven Cardona <cardona.sj@Gmail.com>
2020-04-24 18:32:50 -0500

    update db_transformer

--- db_transformer.py
+++ db_transformer.py
@@ -1,13 +1,16 @@
 import sys
 import logging
 
+print('starting program')
 filename = sys.argv[0]
 fh = open(filename, 'r')
+print('opened file' + filename)
 count = 0
 eof = False
 file = ""
 while count <= 18:
-    fh.readline().strip('\n')
+    line = fh.readline().strip('\n')
+    print(line)
     count += 1
 
 
@@ -16,8 +19,10 @@ while eof == False:
     if line == '-- End of file':
         eof = True
     else:
+        print(line)
         file += line + "\n"
         count += 1
 
 fileWriter = open('prod_sqlscript.sql', 'w')
 fileWriter.write(file)
+print('finished')
