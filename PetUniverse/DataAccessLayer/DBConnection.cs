fcabb1f99219ae8f23fe4272b451f2925baa2768
Steven Cardona <cardona.sj@Gmail.com>
2020-04-30 16:57:08 -0500

    added connnection string for presentation

--- PetUniverse/DataAccessLayer/DBConnection.cs
+++ PetUniverse/DataAccessLayer/DBConnection.cs
@@ -21,22 +21,24 @@ namespace DataTransferObjects
         //Connection string for school
         //@"Data Source=localhost;Initial Catalog=PetUniverseDB;Integrated Security=True"; // Connection string for school
 
+        // Presentation Connect String **DO NOT REMOVE**
+        //@"Data Source=tcp:dbpetuniverse.database.windows.net,1433;Initial Catalog = PetUniverseDB; Persist Security Info=False;User ID = csdadmin; Password=Kirkwood01; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
 
-        /// <summary>
-        /// NAME: Zach Behrensmeyer
-        /// DATE: 2/3/2020
-        /// CHECKED BY: Steven Cardona
-        /// 
-        /// Method that hands back connection string when called
-        /// </summary>
-        /// <remarks>
-        /// UPDATED BY: NA
-        /// UPDATED NA
-        /// CHANGE: NA
-        /// 
-        /// </remarks>
-        /// <returns>SQL Connection String</returns>
-        public static SqlConnection GetConnection()
+    /// <summary>
+    /// NAME: Zach Behrensmeyer
+    /// DATE: 2/3/2020
+    /// CHECKED BY: Steven Cardona
+    /// 
+    /// Method that hands back connection string when called
+    /// </summary>
+    /// <remarks>
+    /// UPDATED BY: NA
+    /// UPDATED NA
+    /// CHANGE: NA
+    /// 
+    /// </remarks>
+    /// <returns>SQL Connection String</returns>
+    public static SqlConnection GetConnection()
         {
             var conn = new SqlConnection(connectionString);
             return conn;
